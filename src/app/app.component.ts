6c80c17fbb40414fbbaa393f3bb1f84898246c17
Alessandro Pinato <alessandro.pinato@dechit.it>
2024-02-14 08:49:13 +0100

    chore: removed autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { appStateGetAccessToken } from 'src/store/app-store.actions';
 import { AppState } from 'src/store/app-store.reducer';
-import { selectAppStateInitialized } from 'src/store/app-store.selectors';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +9,20 @@ import { selectAppStateInitialized } from 'src/store/app-store.selectors';
 })
 export class AppComponent implements OnInit {
 
-  private _clientID = '8eca5ef598c148a9b702abd3cce610ad';
-  private _secret = '400f119ccd6a407ba2d9fa37117f2a30';
+  // private _clientID = '8eca5ef598c148a9b702abd3cce610ad';
+  // private _secret = '400f119ccd6a407ba2d9fa37117f2a30';
 
-  private isInitialized$ = this.store.select(selectAppStateInitialized);
+  // private isInitialized$ = this.store.select(selectAppStateInitialized);
 
 
   constructor(private store: Store<AppState>) {
   }
 
   ngOnInit(): void {
-    this.isInitialized$.subscribe(init => {
-      if (!init) {
-        this.store.dispatch(appStateGetAccessToken({clientId: this._clientID, secret: this._secret }));
-      }
-    });
+    // this.isInitialized$.subscribe(init => {
+    //   if (!init) {
+    //     this.store.dispatch(appStateGetAccessToken({clientId: this._clientID, secret: this._secret }));
+    //   }
+    // });
   }
 }
